8a6a8b11deb895252ef1bf460fbca1db728f9c49
mjewell <mjewell@7b210f86-2f28-11de-afe2-b5feb05456f9>
2009-04-27 13:30:30 +0000

    Function fixes!

--- core/testscripts/call.lua
+++ core/testscripts/call.lua
@@ -1,1 +1,17 @@
-z(1,2,3)
+function double(a)
+    return a*2
+end
+
+function triple(a)
+    return a*3
+end
+
+function sextuple(a)
+    return double(triple(a))
+end
+
+function z(a, b, c)
+    return double(a), triple(b), sextuple(c)
+end
+
+local a, b, c = z(1,2,3)
